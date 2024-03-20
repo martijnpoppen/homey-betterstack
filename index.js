8df1d1aab03ea052a4e6230c6dadc6301f26aa89
Martijn Poppen <m.poppen@ziggo.nl>
2024-03-20 15:34:18 +0100

    homey-betterstack

---
+++ index.js
@@ -0,0 +1,131 @@
+const Homey = require('homey');
+
+const util = require('util');
+
+const { transports, format, Container, addColors } = require('winston');
+const { combine, printf, colorize } = format;
+
+const { Logtail } = require('@logtail/node');
+const { LogtailTransport } = require('@logtail/winston');
+
+const logLevels = {
+    levels: {
+        trace: 0,
+        debug: 1,
+        info: 2,
+        warn: 3,
+        error: 4,
+        fatal: 5,
+        off: 6
+    },
+    colors: {
+        trace: 'blue',
+        debug: 'magenta',
+        info: 'green',
+        warn: 'yellow',
+        error: 'red',
+        fatal: 'red',
+        off: 'grey'
+    }
+};
+
+class HomeyLog extends Homey.App {
+    constructor(...args) {
+        super(...args);
+
+        this.HomeyLogData = {
+            logtail: null
+        };
+
+        this.setupHomeyLogLogtail();
+        this.setupHomeyLogLogger();
+    }
+
+    async setupHomeyLogLogtail() {
+        this.HomeyLogData['HomeyId'] = await this.homey.cloud.getHomeyId();
+
+        if (Homey.env.HOMEY_BETTERSTACK_TOKEN) {
+            this.HomeyLogData.logtail = new Logtail(Homey.env.HOMEY_BETTERSTACK_TOKEN);
+            this.HomeyLogData.logtail.use((data) => ({
+                ...data,
+                homeyId: this.HomeyLogData['HomeyId']
+            }));
+        } else {
+            console.error('HomeyLog: No Homey log token found. Please add HOMEY_BETTERSTACK_TOKEN to your env.json');
+        }
+    }
+
+    async setupHomeyLogLogger() {
+        addColors(logLevels.colors);
+
+        this.HomeyLogData['container'] = new Container();
+
+        this.HomeyLogData['transports'] = [
+            new transports.Console({
+                handleExceptions: true,
+                handleRejections: true,
+                format: combine(
+                    colorize(),
+                    printf(({ level, message }) => {
+                        return `[${level}] ${message}`;
+                    })
+                )
+            })
+        ];
+
+        if (this.HomeyLogData.logtail) {
+            this.HomeyLogData['transports'].push(
+                new LogtailTransport(this.HomeyLogData.logtail, {
+                    handleExceptions: true,
+                    handleRejections: true
+                })
+            );
+        }
+
+        this.HomeyLogData['container'].add('HomeyLogInstance', {
+            levels: logLevels.levels,
+            transports: this.HomeyLogData['transports']
+        });
+
+        this.HomeyLogInstance = this.HomeyLogData['container'].get('HomeyLogInstance');
+    }
+
+    async trace() {
+        this.HomeyLogInstance.log('trace', util.format(...arguments));
+    }
+
+    async debug() {
+        this.HomeyLogInstance.log('debug', util.format(...arguments));
+    }
+
+    async info() {
+        this.HomeyLogInstance.log('info', util.format(...arguments));
+    }
+
+    async log() {
+        this.HomeyLogInstance.log('info', util.format(...arguments));
+    }
+
+    async warn() {
+        this.HomeyLogInstance.log('warn', util.format(...arguments));
+    }
+
+    async error() {
+        this.HomeyLogInstance.log('error', util.format(...arguments));
+    }
+
+    async addHomeyLogChild(scope) {
+        if (!scope.length || !this.HomeyLogData.logtail) {
+            return this.HomeyLogInstance;
+        }
+
+        this.HomeyLogData['container'].add(scope, {
+            levels: logLevels.levels,
+            transports: this.HomeyLogData['transports']
+        });
+
+        return this.HomeyLogData['container'].get(scope);
+    }
+}
+
+module.exports = HomeyLog;
---
+++ readme.md
@@ -0,0 +1,98 @@
+# Homey Betterstack Logger
+📣 Logtail is now part of Better Stack. [Learn more ⇗](https://betterstack.com/press/introducing-better-stack/)
+
+[![Better Stack dashboard](https://github.com/logtail/logtail-js/assets/10132717/96b422e7-3026-49c1-bd45-a946c37211d0)](https://betterstack.com/logs)
+
+## Introduction
+
+This package provides integration between your Homey app and Betterstack logging service. By using this package, you can easily log events, errors, and other information from your Homey app to Betterstack for monitoring and analysis.
+
+## Setup
+
+1. Create an account and project on [Betterstack](https://betterstack.com/logs).
+2. Obtain the `source_token` provided by Betterstack.
+3. Add the `source_token` as `HOMEY_BETTERSTACK_TOKEN` in your `env.json` file.
+
+## Installation
+
+You can install the package via npm:
+
+```bash
+npm install node-homey-betterstack
+```
+
+or 
+
+```bash
+yarn add node-homey-betterstack
+```
+
+## Usage
+### Import the package
+
+```javascript
+
+const HomeyLog = require('node-homey-betterstack');
+```
+
+### Extend the app class
+
+Edit the app class to extend `HomeyLog`:
+
+```javascript
+//From:
+class App extends Homey.App {
+```
+
+
+```javascript
+// To
+class App extends HomeyLog {
+```
+
+### Logging from app.js
+
+Use `this.log` or `this.error` within your `app.js`:
+
+```javascript
+this.log("Event occurred");
+this.error("Error occurred", error);
+```
+
+### Logging from devices and drivers
+
+Use `this.homey.app.log` from within your devices and drivers:
+
+```javascript
+
+this.homey.app.log("Event occurred in device/driver");
+this.homey.app.error("Error occurred in device/driver", error);
+```
+
+## Example
+```javascript
+class App extends HomeyLog {
+    async onInit() {
+        try {       
+            this.log(`${Homey.manifest.id} - ${Homey.manifest.version} started...`);
+        } catch (error) {
+            this.homey.app.log(error);
+        }
+    }
+}
+
+module.exports = App;
+
+```
+
+## Result
+In the terminal:
+
+```bash
+[info] com.test.app - 0.0.1 started...
+```
+
+In Betterstack:
+
+<img src="./assets/image1.png">
+
